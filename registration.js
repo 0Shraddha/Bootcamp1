8d835a9cee884d65a15ab52a95aef5f39d24686a
0Shraddha <shraddhadongol12345@gmail.com>
2021-12-24 09:38:23 +0545

    create JS

---
+++ registration.js
@@ -0,0 +1,29 @@
+console.log("This is registration page");
+
+const registrationForm = document.getElementById("registration form");
+
+console.log(registrationForm);
+
+function getSelected(select) {
+    let selectedValues = []
+    for (optionalIndex in select.options) {
+        if (select.options[optionalIndex].selected) {
+            selectedValues.push(select.options[optionalIndex].value);
+        }
+    } return selectedValues;
+}
+registrationForm.onsubmit = function (event) {
+    event.preventDefault();
+    console.log("I am triggered when submit is pressed");
+    let name = document.getElementById("name").value;
+    let interests = getSelected(document.getElementById("interests"));
+    let dob = document.getElementById("dob").value;
+    let darkMode = document.getElementById("dark-mode").checked;
+    const data = {
+        name,
+        interests,
+        dob,
+        darkMode,
+    };
+    console.log(data);
+};
